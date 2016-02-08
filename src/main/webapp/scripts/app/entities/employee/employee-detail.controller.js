fd53d1966c97429e62397231dfd82e56cc0ac9e6
Sathish Thangaraja <sathish102003@gmail.com>
2016-02-08 15:50:23 +0100

    employee allocation in employee

--- src/main/webapp/scripts/app/entities/employee/employee-detail.controller.js
+++ src/main/webapp/scripts/app/entities/employee/employee-detail.controller.js
@@ -1,16 +1,18 @@
 'use strict';
 
 angular.module('forecastApp')
-    .controller('EmployeeDetailController', function ($scope, $rootScope, $stateParams, entity, Employee, EmployeeAllocation) {
+    .controller('EmployeeDetailController', function ($scope, $rootScope, $stateParams, entity, Employee, EmployeeAllocationSearch) {
         $scope.employee = entity;
         $scope.load = function (id) {
             Employee.get({id: id}, function(result) {
                 $scope.employee = result;
+                $scope.employeeAllocations = EmployeeAllocationSearch.query({query: $scope.employee.name});
             });
         };
         var unsubscribe = $rootScope.$on('forecastApp:employeeUpdate', function(event, result) {
             $scope.employee = result;
         });
         $scope.$on('$destroy', unsubscribe);
+        $scope.load($stateParams.id);
 
     });
