dbc6ee6aa3cb1a731e80580124fb1ed5f411c2fe
Sathish Thangaraja <sathish102003@gmail.com>
2016-02-20 21:46:06 +0100

    EmployeeBillingHours + Employee Holidays are Removed

--- src/main/webapp/scripts/components/entities/employeeBillingHours/employeeBillingHours.search.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('forecastApp')
-    .factory('EmployeeBillingHoursSearch', function ($resource) {
-        return $resource('api/_search/employeeBillingHourss/:query', {}, {
-            'query': { method: 'GET', isArray: true}
-        });
-    });
--- src/main/webapp/scripts/components/entities/employeeHolidays/employeeHolidays.search.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('forecastApp')
-    .factory('EmployeeHolidaysSearch', function ($resource) {
-        return $resource('api/_search/employeeHolidayss/:query', {}, {
-            'query': { method: 'GET', isArray: true}
-        });
-    });
