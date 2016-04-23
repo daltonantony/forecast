670da7fc26cf3d89bdee5c95c7ceee8d1867aba6
Jegannathan <rknjegannathan@gmail.com>
2016-04-23 21:27:25 +0200

    Download Forecast - Added additional sheet in the excel with a different format showing forecast hours

--- src/main/java/com/opensolutions/forecast/service/util/EmployeeHoursHelper.java
+++ src/main/java/com/opensolutions/forecast/service/util/EmployeeHoursHelper.java
@@ -1,5 +1,6 @@
 package com.opensolutions.forecast.service.util;
 
+import java.io.FileOutputStream;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
@@ -7,11 +8,7 @@ import java.time.Year;
 import java.time.YearMonth;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFFont;
@@ -19,6 +16,7 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
 
 import com.opensolutions.forecast.domain.DaysOfMonth;
@@ -29,6 +27,46 @@ import com.opensolutions.forecast.domain.Employee;
  */
 public class EmployeeHoursHelper {
 
+    /*private static final EnumSet<DayOfWeek> WEEKEND = EnumSet.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
+
+    public static void main(final String[] args) {
+        final Map<Employee, Map<LocalDate, List<DaysOfMonth>>> employeeHours = new HashMap<>();
+        Employee emp = new Employee();
+        emp.setId(1L);
+        emp.setAssociateId(224801L);
+        emp.setName("First Name, Last Name 1");
+        emp.setDomain("Project A");
+        Map<LocalDate, List<DaysOfMonth>> empHours = new HashMap<>();
+        empHours.put(LocalDate.now(), addHours(LocalDate.now()));
+        empHours.put(LocalDate.now().plusMonths(1), addHours(LocalDate.now().plusMonths(1)));
+        empHours.put(LocalDate.now().plusMonths(2), addHours(LocalDate.now().plusMonths(2)));
+        empHours.put(LocalDate.now().plusMonths(3), addHours(LocalDate.now().plusMonths(3)));
+        employeeHours.put(emp, empHours);
+        emp = new Employee();
+        emp.setId(2L);
+        emp.setAssociateId(224488L);
+        emp.setName("First Name, Last Name 2");
+        emp.setDomain("Project B");
+        empHours = new HashMap<>();
+        empHours.put(LocalDate.now().plusMonths(1), addHours(LocalDate.now().plusMonths(1)));
+        employeeHours.put(emp, empHours);
+        new EmployeeHoursHelper().writeHoursInWorkbook(employeeHours);
+    }
+
+    private static List<DaysOfMonth> addHours(final LocalDate date) {
+        final List<DaysOfMonth> dom = new ArrayList<>();
+        for (int i = 1; i <= date.with(TemporalAdjusters.lastDayOfMonth()).getDayOfMonth(); i++) {
+            final DaysOfMonth daysOfMonth = new DaysOfMonth();
+            daysOfMonth.setDay(i);
+            daysOfMonth.setHoliday(WEEKEND.contains(date.withDayOfMonth(i).getDayOfWeek()));
+            if (i % 3 == 0) {
+                daysOfMonth.setSelected(Boolean.TRUE);
+            }
+            dom.add(daysOfMonth);
+        }
+        return dom;
+    }*/
+
     /**
      * Write hours in workbook.
      *
@@ -37,29 +75,103 @@ public class EmployeeHoursHelper {
      */
     public HSSFWorkbook writeHoursInWorkbook(final Map<Employee, Map<LocalDate, List<DaysOfMonth>>> employeeHours) {
         final HSSFWorkbook workbook = new HSSFWorkbook();
-        final HSSFSheet sheet = workbook.createSheet("Forecast");
-
         final HSSFCellStyle headerStyle = workbook.createCellStyle();
         final HSSFFont headerFont = workbook.createFont();
         headerFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
         headerStyle.setFont(headerFont);
+        headerStyle.setAlignment(CellStyle.ALIGN_CENTER);
 
         final HSSFCellStyle employeeStyle = workbook.createCellStyle();
         final HSSFFont employeeFont = workbook.createFont();
         employeeFont.setColor(HSSFColor.BLUE.index);
         employeeFont.setFontName(HSSFFont.FONT_ARIAL);
         employeeStyle.setFont(employeeFont);
+        employeeStyle.setAlignment(CellStyle.ALIGN_RIGHT);
 
         final HSSFCellStyle forecastStyle = workbook.createCellStyle();
-        forecastStyle.setFillForegroundColor(HSSFColor.LIME.index);
-        forecastStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+        // forecastStyle.setFillForegroundColor(HSSFColor.LIME.index);
+        // forecastStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
         final HSSFFont forecastFont = workbook.createFont();
         forecastFont.setColor(HSSFColor.RED.index);
         forecastStyle.setFont(forecastFont);
+        forecastStyle.setAlignment(CellStyle.ALIGN_CENTER);
 
         final HSSFCellStyle holidayStyle = workbook.createCellStyle();
         holidayStyle.setFillForegroundColor(HSSFColor.GREY_25_PERCENT.index);
         holidayStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+
+        createSheetWithForecastA(employeeHours, workbook, headerStyle, employeeStyle, forecastStyle, holidayStyle);
+        createSheetWithForecastB(employeeHours, workbook, headerStyle, employeeStyle, forecastStyle, holidayStyle);
+        autoSizeColumns(workbook);
+
+        /*try { final FileOutputStream outputStream = new FileOutputStream("W:/test.xls");
+        workbook.write(outputStream); } catch (final Exception e) { System.out.println(e); }*/
+
+        return workbook;
+    }
+
+    private void createSheetWithForecastA(Map<Employee, Map<LocalDate, List<DaysOfMonth>>> employeeHours, HSSFWorkbook workbook, HSSFCellStyle headerStyle, HSSFCellStyle employeeStyle, HSSFCellStyle forecastStyle, HSSFCellStyle holidayStyle) {
+        final HSSFSheet sheet= workbook.createSheet("Forecast A");
+        int rowCount = 0;
+        int columnCount = 0;
+        Row row = sheet.createRow(rowCount++);
+        Cell headerCell = row.createCell(columnCount++);
+        headerCell.setCellValue("ID");
+        headerCell.setCellStyle(headerStyle);
+        headerCell = row.createCell(columnCount++);
+        headerCell.setCellValue("AssociateID");
+        headerCell.setCellStyle(headerStyle);
+        headerCell = row.createCell(columnCount++);
+        headerCell.setCellValue("LeaveDate");
+        headerCell.setCellStyle(headerStyle);
+        headerCell = row.createCell(columnCount++);
+        headerCell.setCellValue("Days");
+        headerCell.setCellStyle(headerStyle);
+        headerCell = row.createCell(columnCount++);
+        headerCell.setCellValue("AssociateName");
+        headerCell.setCellStyle(headerStyle);
+        headerCell = row.createCell(columnCount++);
+        headerCell.setCellValue("ProjectName");
+        headerCell.setCellStyle(headerStyle);
+        headerCell = row.createCell(columnCount++);
+        headerCell.setCellValue("DomainName");
+        headerCell.setCellStyle(headerStyle);
+
+        for (final Map.Entry<Employee, Map<LocalDate, List<DaysOfMonth>>> entry : employeeHours.entrySet()) {
+            for (final Map.Entry<LocalDate, List<DaysOfMonth>> empHours : entry.getValue().entrySet()) {
+                final LocalDate date = empHours.getKey();
+                //if (date.getMonthValue() > LocalDate.now().getMonthValue()) {
+                    final List<DaysOfMonth> daysOfMonth = empHours.getValue();
+                    for (final DaysOfMonth dayOfMonth : daysOfMonth) {
+                        columnCount = 0;
+                        if (dayOfMonth.isSelected()) {
+                            row = sheet.createRow(rowCount++);
+                            Cell cell = row.createCell(columnCount++);
+                            cell.setCellValue(entry.getKey().getId());
+                            cell = row.createCell(columnCount++);
+                            cell.setCellValue(entry.getKey().getAssociateId());
+                            cell.setCellStyle(employeeStyle);
+                            cell = row.createCell(columnCount++);
+                            cell.setCellValue(date.withDayOfMonth(dayOfMonth.getDay()).toString());
+                            cell.setCellStyle(forecastStyle);
+                            cell = row.createCell(columnCount++);
+                            cell.setCellValue(1);
+                            cell = row.createCell(columnCount++);
+                            cell.setCellValue(entry.getKey().getName());
+                            cell.setCellStyle(employeeStyle);
+                            cell = row.createCell(columnCount++);
+                            cell.setCellValue(entry.getKey().getDomain());
+                            cell = row.createCell(columnCount++);
+                            cell.setCellValue("");
+                        }
+                    }
+                //}
+            }
+        }
+    }
+
+    private void createSheetWithForecastB(Map<Employee, Map<LocalDate, List<DaysOfMonth>>> employeeHours, HSSFWorkbook workbook, HSSFCellStyle headerStyle, HSSFCellStyle employeeStyle, HSSFCellStyle forecastStyle, HSSFCellStyle holidayStyle) {
+        final HSSFSheet sheet = workbook.createSheet("Forecast B");
 
         int rowCount = 0;
         int columnCount = 0;
@@ -99,12 +211,6 @@ public class EmployeeHoursHelper {
                 }
             }
         }
-        autoSizeColumns(workbook);
-        /*
-         * try { final FileOutputStream outputStream = new FileOutputStream("W:/test.xls");
-         * workbook.write(outputStream); } catch (final Exception e) { System.out.println(e); }
-         */
-        return workbook;
     }
 
     private void createDatesOfMonth(final Row row, final LocalDate date, int columnCount,
